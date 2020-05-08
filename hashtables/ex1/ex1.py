204cae6a9f56f99a7b695976ad04bbd3e6d85c1c
Code4Blessings <robinwarden@code4blessings.com>
2020-05-08 14:02:14 -0400

    Updated ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,13 +7,12 @@ def get_indices_of_item_weights(weights, length, limit):
     #Create a dictionary and print all of the indices 
     weight_dict = dict()
     #Need to loop through all the values in the length of the dictionary and return 2 indices whose value equals the limit
-    for i in range(0, length):
-        for weight_1 in range(0, length):  # index
+        for weight_1 in range(0, length):  
             (weights[weight_1], weight_1)
-    # for the second weight in the range from 0 to length
+   
     for weight_2 in range(0, length):
-        # subtract [weight value] from limit and assign result to matching_weight
-        matching_weight = limit - weights[weight_2]
-        # The matching weight and assign to found_weight
+       
+        result = limit - weights[weight_2] #the result should 
+      
            return None
 
