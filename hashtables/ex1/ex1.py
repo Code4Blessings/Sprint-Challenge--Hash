f3ce4e67c87b691cef9628073b82ff1afdd68518
Code4Blessings <robinwarden@code4blessings.com>
2020-05-09 21:15:19 -0400

    Updated ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,6 +1,4 @@
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_retrieve,)
+from hashtables import (HashTable, put, get)
 
 def get_indices_of_item_weights(weights, length, limit):
 
@@ -9,17 +7,17 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Set the capacity to 21
     ht = HashTable(21)
-    # for the first weight in the range from 0 to length
+    # Set the range from o to length
     for i in range(0, length):  # index
-        # insert hashtable, [weight key], and weight value
-        hash_table_insert(ht, weights[i], i)
-    # for the second weight in the range from 0 to length
+        # insert hashtable, with the weight value
+        put(ht, weights[i], i)
+    # Set the second weight range from 0 to length
     for j in range(0, length):
         # subtract [j] from limit and assign result to limit_weight
         limit_weight = limit - weights[j]
-        # retrieve the matching wight from the hash table and assign to match_weight
-        match_weight = hash_table_retrieve(ht, limit_weight)
-        # if we found a weight ...
+        # get the matching wight from the hash table and assign to match_weight
+        match_weight = get(ht, limit_weight)
+        # If the weight was found
         if match_weight:
             # return the match weight and the other weight value
             return (match_weight, j)
